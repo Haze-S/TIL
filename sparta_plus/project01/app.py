5794228567b74ca50085825323b8fdbd5305abc1
Haze-S <skdml20022@gmail.com>
2021-11-13 18:00:00 +0900

    .

---
+++ sparta_plus/project01/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta_plus_week1
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive
+    }
+
+    db.diary.insert_one(doc)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
