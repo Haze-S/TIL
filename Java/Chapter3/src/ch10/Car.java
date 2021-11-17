08b4682f6790304e19dd5f2c285e8b0c8404bd57
Haze-S <skdml20022@gmail.com>
2021-11-17 18:00:00 +0900

    추상클래스

---
+++ Java/Chapter3/src/ch10/Car.java
@@ -0,0 +1,27 @@
+package ch10;
+
+public abstract class Car {
+	
+	public abstract void drive();
+	public abstract void stop();
+	public abstract void wiper();
+	
+	public void startCar() {
+		System.out.println("시동을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("시동을 끕니다.");
+	}
+	
+	public void washCar() {} // 구현 내용이 없음, 추상 메서드 X, 필요한 경우에 재정의해서 사용한다.
+	
+	final public void run() {
+		startCar();
+		drive();
+		wiper();
+		stop();
+		turnOff();
+		washCar();
+	}
+}
