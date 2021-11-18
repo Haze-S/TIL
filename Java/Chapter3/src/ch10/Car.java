fdaf29704ad9e5e68307fc0f7c63020a28985786
Haze-S <skdml20022@gmail.com>
2021-11-18 14:00:00 +0900

    인터페이스

--- Java/Chapter3/src/ch10/Car.java
+++ Java/Chapter3/src/ch10/Car.java
@@ -14,7 +14,7 @@ public abstract class Car {
 		System.out.println("시동을 끕니다.");
 	}
 	
-	public void washCar() {} // 구현 내용이 없음, 추상 메서드 X, 필요한 경우에 재정의해서 사용한다.
+	public void washCar() {} // Hook 메서드 : 구현 내용이 없음, 추상 메서드 X, 필요한 경우에 재정의해서 사용한다.
 	
 	final public void run() {
 		startCar();
