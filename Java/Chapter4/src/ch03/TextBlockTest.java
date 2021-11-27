f5ce9d68a09836cc665db44b029f8ec5bc956626
Haze-S <skdml20022@gmail.com>
2021-11-27 13:59:56 +0900

    class

---
+++ Java/Chapter4/src/ch03/TextBlockTest.java
@@ -0,0 +1,25 @@
+package ch03;
+
+public class TextBlockTest {
+
+	public static void main(String[] args) {
+
+		String textBlocks = """
+				Hello,
+				hi,
+				how are you?
+				""";
+		System.out.println(textBlocks);
+		System.out.println(getBlockOfHtml());
+	}
+	public static String getBlockOfHtml() {
+		return """
+				<html>
+					<body>
+						<span>example text</span>
+					</body>
+				</html>
+			""";
+	}
+
+}
